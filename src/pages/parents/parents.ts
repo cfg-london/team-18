ed2c3dcedfaf0fa3d35af85b14b475c1e6fe57c1
Mantas-Skackauskas <smantaslt@gmail.com>
2017-11-18 11:05:35 +0000

    final

--- src/pages/parents/parents.ts
+++ src/pages/parents/parents.ts
@@ -9,27 +9,30 @@ import { AlertController } from 'ionic-angular';
 
 
 @Component({
-  selector: 'page-parents',
-  templateUrl: 'parents.html'
+ selector: 'page-parents',
+ templateUrl: 'parents.html'
 })
 
 export class Parents {
 
 
-  constructor(public navCtrl: NavController,private alertCtrl: AlertController) {
+ constructor(public navCtrl: NavController,private alertCtrl: AlertController) {
 
-  }
+ }
 
 
-  presentAlert() {
-    let alert = this.alertCtrl.create({
-      title: 'title here',
-      subTitle: 'Add text here about this person right over here haha ',
-      buttons: ['Thanks!']
-    });
+ presentAlert() {
+   let alert = this.alertCtrl.create({
+     title: 'Our Story',
+     subTitle: 'When we find out that Philippe had cancer we were devasted. He is the benjamin of the family so we were really sad. Thankfully the doctors helped us throughout this whole journey of faith and courage. As parents it is our duty to make our kids still feel like they normally growing up despite the disease.',
+     buttons: ['Thanks!']
+   });
 
-    alert.present();
-  }
+   alert.present();
+ }
+
+
+
 
 
 
